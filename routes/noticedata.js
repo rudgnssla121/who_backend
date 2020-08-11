7fddd88d2b2ab32db01520275d29107260b25bf0
rudgnssla121 <rudgnssla121@naver.com>
2020-08-12 00:32:10 +0900

    notice.js 에서 delete modify  구현, modify는 할때마다 id auto_increment = 1로 초기화, delete는 id delete한 이후로 하나씩 빼고 auto_increment = 1로 초기화 , 항상 리스트 순서 맞게 구현

--- routes/noticedata.js
+++ routes/noticedata.js
@@ -5,7 +5,7 @@ var connection = mysql_dbc.init();
 
 /* GET home page. */
 router.get('/listview', function(req, res, next) {
-    var sql = 'SELECT DATE_FORMAT(time,\'%y-%m-%d\') AS date, DATE_FORMAT(time,\'%h:%i\') AS time, username, name, content  FROM notice_board';
+    var sql = 'SELECT id, DATE_FORMAT(time,\'%y-%m-%d\') AS date, DATE_FORMAT(time,\'%h:%i\') AS time, username, name, content  FROM notice_board';
 
     //console.log(PASSWORD(password));
 
@@ -13,6 +13,7 @@ router.get('/listview', function(req, res, next) {
         if (error) {
             console.log('query error : ' + error);
         } else {
+            //console.log(rows);
             res.send(rows);
         }
     });
@@ -22,7 +23,7 @@ router.get('/listview', function(req, res, next) {
 router.get('/listfind/:index', function(req, res, next) {
     const index = parseInt(req.params.index, 10);
     var sql = 'SELECT DATE_FORMAT(time,\'%y-%m-%d\') AS date, DATE_FORMAT(time,\'%h:%i:%s\') AS time, username, name, content  FROM notice_board';
-    //console.log(index);
+    //nomconsole.log(index);
     //console.log(PASSWORD(password));
 
     connection.query(sql, function (error, rows, fields) {
@@ -41,6 +42,15 @@ router.post('/listadd', function (req,res,next){
     var content = req.body.content;
     var params = [username, name, content];
     var sql = 'INSERT INTO notice_board (username, name, content) VALUES(?, ?, ?)';
+    var sql1 = 'ALTER TABLE notice_board AUTO_INCREMENT=1;'
+    connection.query(sql1, function(err, rows, fields){
+        if(err) {
+            console.log(err);
+        }
+        else{
+        }
+
+    });
     connection.query(sql, params, function(err, rows, fields){
         if(err) {
             console.log(err);
@@ -53,7 +63,58 @@ router.post('/listadd', function (req,res,next){
 
     });
 
+});
 
+router.post('/listmodify', function (req,res,next){
+    var name = req.body.noticecontent.name;
+    var content = req.body.noticecontent.content;
+    const index = parseInt(req.body.noticecontent.index, 10);
+    var params = [name, content, index+1];
+    var sql = 'UPDATE notice_board SET name = ?, content = ? WHERE id = ?';
+    connection.query(sql,params ,function(err, rows, fields){
+        if(err) {
+            console.log(err);
+        }
+        else{
+            res.json({
+                message: '수정됐습니다!'
+            });
+        }
+
+    });
+});
+
+router.get('/listDelete/:index', function (req,res,next){
+    const index = parseInt(req.params.index, 10);
+
+    var sql = 'DELETE from notice_board WHERE id = ?';
+    connection.query(sql, index+1,function(err, rows, fields){
+        if(err) {
+            console.log(err);
+        }
+        else{
+            console.log('삭제됐습니다');
+        }
+
+    });
+    sql = 'ALTER TABLE notice_board AUTO_INCREMENT=1;'
+    connection.query(sql,function(err, rows, fields){
+        if(err) {
+            console.log(err);
+        }
+        else{
+        }
+
+    });
+    sql = 'UPDATE notice_board SET id = id - 1 WHERE id > ? AND id > 1';
+    connection.query(sql, index ,function(err, rows, fields){
+        if(err) {
+            console.log(err);
+        }
+        else{
+        }
+
+    });
 });
 
 
